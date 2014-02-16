34ebcd5a7665be26cbf87d670f2d12f2e2c9ce08
Roeland Reyniers <roeland@bythepixel.com>
2014-02-15 20:54:12 -0700

    Add Photo resource. Some cleanup.

---
+++ app/database/migrations/2014_02_16_033631_create_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photos', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('file_name');
+			$table->string('title');
+			$table->string('desc');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photos');
+	}
+
+}
