56a3fe8f9b480a434b05d6737821876faeb4ad9e
Roeland Reyniers <roeland@bythepixel.com>
2014-03-22 17:25:04 -0600

    Add asset management.

--- app/views/layouts/front.blade.php
+++ app/views/layouts/front.blade.php
@@ -1,5 +1,11 @@
 <html>
+
+  <head>
+    <?= stylesheet_link_tag() ?>
+    <?= javascript_include_tag() ?>
+  </head>
   <body>
+
     <h1>Lilly and Roeland . com</h1>
     <div class="container">
         @yield('content')
